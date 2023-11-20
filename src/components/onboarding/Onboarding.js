a45b51616c45d212afa84ba86188d3752a2d7cd6
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-11-20 11:51:01 +0700

    Add onboarding feature with slides

--- src/components/onboarding/Onboarding.js
+++ src/components/onboarding/Onboarding.js
@@ -1,43 +1,129 @@
-import { StyleSheet } from "react-native";
-import { OnboardFlow, PrimaryButton } from 'react-native-onboard';
-
-
+import { Animated, SafeAreaView, StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { Dimensions } from "react-native";
+import slides from "./slides";
+import { useEffect, useState, useRef } from "react";
 
 export default function Onboarding() {
+  const [completed, setCompleted] = useState(false);
 
-  return (
-    <OnboardFlow
-      pages={[
-        {
-          title: 'Unleash Your Inner Chef',
-          subtitle: 'From ingredients to meal - your delicious journey begins here',
-          imageUri: 'https://frigade.com/img/example1.png',
-        },
-        {
-          title: 'Scan & Savor',
-          subtitle: 'Scan your ingredients and get your recipes effortlessly',
-          imageUri: 'https://frigade.com/img/example2.png',
-        },
-        {
-          title: 'Snap, Save, Share',
-          subtitle: 'Discover, bookmark and share delicious recipes',
-          imageUri: 'https://frigade.com/img/example2.png',
-        }
-      ]
+  const scrollX = new Animated.Value(0);
+  const scrollRef = useRef();
+  useEffect(() => {
+    scrollX.addListener(({ value }) => {
+      if (Math.floor(value / Dimensions.get('window').width) === slides.length - 1) {
+        setCompleted(true);
       }
-      type={'fullscreen'}
-      titleStyle={styles.titleStyle}
-      subtitleStyle={styles.subtitleStyle}
-      paginationSelectedColor={'#FCF0DA'}
-      primaryButtonStyle={styles.buttonStyle}
-      primaryButtonTextStyle={styles.buttonTextStyle}
-      showDismissButton={true}
-      dismissButtonStyle={styles.buttonStyle}
-    />
+    })
+    return () => scrollX.removeListener();
+  }, [])
+
+
+
+
+  function renderSlides() {
+    return (<Animated.ScrollView
+      horizontal
+      pagingEnabled
+      scrollEnabled
+      snapToAlignment={'center'}
+      decelerationRate={0}
+      showsHorizontalScrollIndicator={false}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+        { useNativeDriver: false }
+      )}
+      ref={scrollRef}
+    >
+      {slides.map((slide, index) => (
+        <View key={index} style={styles.container}>
+          {/* skip button */}
+          <View style={styles.skipContain}>
+            <Text style={styles.skipStyle}>Skip</Text>
+          </View>
+          <View style={styles.imageContain}>
+            <Image source={
+              slide.image
+            } style={styles.imageStyle} />
+          </View>
+          <View style={styles.textContain}>
+            <Text style={styles.titleStyle}>{slide.title}</Text>
+            <View style={styles.subtitleContain}>
+              <Text style={styles.subtitleStyle}>{slide.subtitle}</Text>
+            </View></View>
+
+          {/* button */}
+          <View style={styles.buttonContain}>
+            <Pressable
+              style={styles.buttonStyle}
+              onPress={() => {
+                if (completed) {
+                  console.warn('completed');
+                }
+                setCompleted(false);
+                // next slide
+                scrollRef.current.scrollTo({
+                  x: Dimensions.get('window').width * (index + 1),
+                  animated: true,
+                })
+              }}
+            >
+              <Text style={styles.buttonTextStyle}>{completed ? 'Get Started' : 'Next'}</Text>
+            </Pressable>
+          </View>
+        </View>
+      ))
+      }
+    </Animated.ScrollView>
+    )
+  }
+
+  function renderDots() {
+    const dotPosition = Animated.divide(scrollX, Dimensions.get('window').width);
+    return (<View style={styles.dotsContainer}>
+      {slides.map((_, index) => {
+        const dotSize = dotPosition.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [10, 70, 10],
+          extrapolate: 'clamp',
+        });
+        return (<Animated.View key={`dot-${index}`} style={[styles.dot, { width: dotSize }]}>
+
+        </Animated.View>)
+      })}
+    </View>)
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>{renderSlides()}</View>
+      <View style={styles.buttonContainer}>
+        {renderDots()}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContain: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+    marginTop: 80,
+  },
+  textContain: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
   titleStyle: {
     fontSize: 36,
     fontWeight: 'bold',
@@ -47,19 +133,62 @@ const styles = StyleSheet.create({
   subtitleStyle: {
     fontSize: 24,
     color: '#000',
-    fontFamily: 'Judson',
+    fontFamily: 'Judson, sans-serif',
   },
-  buttonStyle: {
-    backgroundColor: '#FCF0DA',
-    shadowColor: '#000',
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    shadowOffset: { width: 3, height: 3 },
+  imageStyle: {
+    width: "100%",
+    height: "100%",
+    resizeMode: 'contain',
   },
   buttonTextStyle: {
     color: '#000',
     fontSize: 20,
     fontFamily: 'Judson',
     fontWeight: 'bold',
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  dot: {
+    height: 8,
+    borderRadius: 5,
+    backgroundColor: '#FCF0DA',
+    marginHorizontal: 6,
+  },
+
+  skipContain: {
+    position: 'absolute',
+    top: 80,
+    right: 30,
+  },
+  skipStyle: {
+    fontSize: 17,
+    color: '#000',
+    fontStyle: 'italic',
+    fontFamily: 'Judson, sans-serif',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    borderRadius: 50,
+    backgroundColor: '#FCF0DA',
+  },
+  buttonContain: {
+    width: "90%",
+    position: 'absolute',
+    bottom: 30,
+    color: '#000',
+    height: 50,
+  },
+  subtitleContain: {
+    marginTop: 40,
+    width: "80%",
   },
 });
---
+++ src/components/onboarding/slides.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: 'Unleash Your Inner Chef',
+    subtitle: 'From ingredients to meal - your delicious journey begins here',
+    image: require('../../../assets/Pic_1.png'),
+  },
+  {
+    title: 'Scan & Savor',
+    subtitle: 'Scan your ingredients and get your recipes effortlessly',
+    image: require('../../../assets/Pic_2.png')
+  },
+  {
+    title: 'Snap, Save, Share',
+    subtitle: 'Discover, bookmark and share delicious recipes',
+    image: require('../../../assets/Pic_3.png')
+  }
+]
