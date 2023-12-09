97d9bbbe9064a8bf62c32c9ab6531398ed19fd33
Minh Thuan <thuan17082002@gmail.com>
2023-12-09 19:53:06 +0700

    Add queryClient

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 // import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Authencitation } from './StackNavigator';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Authencitation, Home } from './StackNavigator';
 import { Onboard } from './StackNavigator';
+const queryClient = new QueryClient();
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Onboarding" component={Onboard} options={{ headerShown: false }} />
-        <Stack.Screen name="Authencitation" component={Authencitation} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Onboarding" component={Onboard} options={{ headerShown: false }} />
+          <Stack.Screen name="Authencitation" component={Authencitation} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
