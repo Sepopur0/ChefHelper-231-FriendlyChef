8392e924795350130b06c773b4bd1e891fbb0e93
Phú Nguyễn <trongphu012345@gmail.com>
2024-01-03 10:48:20 +0700

    feat(swiper fix): fix swiper

--- src/pages/detailRecipe.jsx
+++ src/pages/detailRecipe.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   SafeAreaView,
@@ -6,18 +6,45 @@ import {
   Text,
   Image,
   ScrollView,
-  Animated,
 } from "react-native";
 import { RecipeDetailStyle } from "../style/detailRecipeStyle.js";
 import RecipeGuide from "../components/recipe-guide.jsx";
 import BackTopBarNavigator from "../components/backTopBarNavigator.jsx";
 import useRecipeById from "../services/recipe/getRecipeById.js";
-
 import Swiper from "react-native-swiper";
 
 const RecipeDetail = ({ route }) => {
   const id = route?.params?.recipeId;
   const { data: recipe, error, isLoading } = useRecipeById(id);
+  const [swiperIndex, setSwiperIndex] = useState(0);
+
+  useEffect(() => {
+    let swiperInterval;
+
+    const prefetchImages = async () => {
+      try {
+        await Promise.all(recipe.images.map((image) => Image.prefetch(image)));
+        setSwiperIndex(0); // Reset swiperIndex after images are prefetched
+        swiperInterval = setInterval(() => {
+          setSwiperIndex((prevIndex) => (prevIndex + 1) % recipe.images.length);
+        }, 3000);
+      } catch (error) {
+        console.error("Lỗi tải trước ảnh:", error);
+      }
+    };
+
+    if (recipe && recipe.images && recipe.images.length > 0) {
+      prefetchImages();
+    }
+
+    return () => {
+      clearInterval(swiperInterval);
+    };
+  }, [recipe]);
+
+  const onIndexChanged = (index) => {
+    setSwiperIndex(index);
+  };
 
   if (isLoading) {
     return (
@@ -39,7 +66,7 @@ const RecipeDetail = ({ route }) => {
     );
   }
 
-  if (!recipe) {
+  if (!recipe || !recipe.images || recipe.images.length === 0) {
     return (
       <SafeAreaView style={RecipeDetailStyle.container}>
         <StatusBar style={RecipeDetailStyle.statusBar} />
@@ -49,8 +76,6 @@ const RecipeDetail = ({ route }) => {
       </SafeAreaView>
     );
   }
-
-  console.log({ recipe });
 
   return (
     <SafeAreaView style={RecipeDetailStyle.container}>
@@ -66,25 +91,22 @@ const RecipeDetail = ({ route }) => {
           <Text style={RecipeDetailStyle.calorieText}>
             {recipe.calorie} calories
           </Text>
+          
           <Swiper
             style={RecipeDetailStyle.imageContainer}
             showsButtons={false}
-            autoplay={true}
             dotStyle={RecipeDetailStyle.dotStyle}
             activeDotStyle={RecipeDetailStyle.activeDotStyle}
+            index={swiperIndex}
+            onIndexChanged={onIndexChanged}
           >
-            {recipe.images.map((image, index) => {
-              console.log({ image });
-              return (
-                <View key={index}>
-                  <Image
-                    style={RecipeDetailStyle.image}
-                    source={{ uri: image }}
-                  />
-                </View>
-              );
-            })}
+            {recipe.images.map((image, index) => (
+              <View key={index}>
+                <Image style={RecipeDetailStyle.image} source={{ uri: image }} />
+              </View>
+            ))}
           </Swiper>
+
           <RecipeGuide guide={recipe.guide} />
         </View>
       </ScrollView>
@@ -93,3 +115,4 @@ const RecipeDetail = ({ route }) => {
 };
 
 export default RecipeDetail;
+
